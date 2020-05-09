af591143ecfedfca72793b40edb7979d5acf5091
magicbart <magicbart@gmail.com>
2020-05-09 15:36:45 +0200

    Add function create to use on another bundle

--- Repository/MessageRepository.php
+++ Repository/MessageRepository.php
@@ -3,9 +3,23 @@
 namespace ProjetNormandie\MessageBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use ProjetNormandie\MessageBundle\Entity\Message;
 
 class MessageRepository extends EntityRepository
 {
-
-
+    /**
+     * @param $data
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function create($data)
+    {
+        $message = new Message();
+        $message->setType(isset($data['type']) ? $data['type'] : 'DEFAULT');
+        $message->setObject($data['object']);
+        $message->setMessage($data['message']);
+        $message->setSender($data['sender']);
+        $message->setRecipient($data['recipient']);
+        $this->_em->persist($message);
+        $this->_em->flush();
+    }
 }
